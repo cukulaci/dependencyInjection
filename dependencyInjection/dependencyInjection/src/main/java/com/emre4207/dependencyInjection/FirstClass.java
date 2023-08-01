4e0b377abacf58f5b27dcff50a265342014997ea
Emre SAGLAM <69041945+cukulaci@users.noreply.github.com>
2023-08-01 21:14:02 +0300

    Add files via upload

---
+++ dependencyInjection/dependencyInjection/src/main/java/com/emre4207/dependencyInjection/FirstClass.java
@@ -0,0 +1,12 @@
+package com.emre4207.dependencyInjection;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirstClass {
+
+	public String getName() {
+		return "First Class";
+	}
+	
+}
---
+++ dependencyInjection/dependencyInjection/src/main/java/com/emre4207/dependencyInjection/SecondClass.java
@@ -0,0 +1,9 @@
+package com.emre4207.dependencyInjection;
+
+public class SecondClass {
+	
+	public String getName() {
+		return "Second Class";
+	}
+
+}
